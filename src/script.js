51c3a9efb98092c289bd432cbbaaa3999144afbe
AugmentedDuck <anders.m.matzen@gmail.com>
2023-12-06 22:13:49 +0100

    Trying to figure out spotify

--- src/script.js
+++ src/script.js
@@ -1,3 +1,38 @@
 function RedirectToSpotify() {
     window.location.href = "https://open.spotify.com"
 }
+
+// SPOTIFYS EXAMPLE CODE
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = '';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
+
+async function LogTop() {
+    let topTracks = await getTopTracks();
+    
+    console.log(
+      topTracks?.map(
+        ({name, artists}) =>
+          `${name} by ${artists.map(artist => artist.name).join(', ')}`
+      )
+    );
+}
+
+LogTop();
